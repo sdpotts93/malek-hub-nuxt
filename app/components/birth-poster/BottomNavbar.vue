1b6c23f37f8d51438536e26c5e4efe6ff5d35d0b
Steven Potts <stevenpotts@Stevens-MacBook-Pro.local>
2025-12-30 00:30:27 -0600

    mobile WIP

--- app/components/birth-poster/BottomNavbar.vue
+++ app/components/birth-poster/BottomNavbar.vue
@@ -16,25 +16,10 @@ defineProps<Props>()
 const emit = defineEmits<{
   select: [panel: PanelType]
 }>()
-
-const uiStore = useUIStore()
 </script>
 
 <template>
   <nav class="bottom-navbar">
-    <!-- Home button -->
-    <button
-      class="bottom-navbar__item bottom-navbar__item--home"
-      @click="uiStore.openMobileNavWrapper('home')"
-    >
-      <span class="bottom-navbar__icon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-          <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
-          <polyline points="9 22 9 12 15 12 15 22"/>
-        </svg>
-      </span>
-    </button>
-
     <!-- Panel navigation items -->
     <button
       v-for="item in items"
@@ -105,14 +90,6 @@ const uiStore = useUIStore()
 
     &--active {
       color: $color-brand;
-    }
-
-    &--home {
-      color: $color-text-muted;
-
-      @include hover {
-        color: $color-text-primary;
-      }
     }
   }
 
